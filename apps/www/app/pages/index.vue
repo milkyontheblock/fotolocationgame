3152dd58d856f663b51e47e19368eb2946ad7f04
Milky <43879645+milkyontheblock@users.noreply.github.com>
2026-07-17 11:30:46 +0200

    Initial commit

---
+++ apps/www/app/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { authClient } from '~~/lib/auth-client'
+
+const { data: session } = await authClient.useSession(useFetch)
+</script>
+
+<template>
+  <div v-if="session">
+    <p>Welcome, {{ session.user.name }}</p>
+    <button @click="authClient.signOut()">
+      Sign out
+    </button>
+  </div>
+
+  <button
+    v-else
+    @click="authClient.signIn.social({ provider: 'google' })"
+  >
+    Continue with Google
+  </button>
+</template>
